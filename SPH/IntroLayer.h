5b42edd7ba8ff4b99a46147d022c6a31391cef88
Vasiliy Yanushevich <vavius@vasiliys-imac.unmedia.ru>
2012-11-02 14:50:01 +0400

    Initial Commit

---
+++ SPH/IntroLayer.h
@@ -0,0 +1,21 @@
+//
+//  IntroLayer.h
+//  SPH
+//
+//  Created by Vasiliy Yanushevich on 11/2/12.
+//  Copyright Vasiliy Yanushevich 2012. All rights reserved.
+//
+
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface IntroLayer : CCLayer
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
---
+++ SPH/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  SPH
+//
+//  Created by Vasiliy Yanushevich on 11/2/12.
+//  Copyright Vasiliy Yanushevich 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
